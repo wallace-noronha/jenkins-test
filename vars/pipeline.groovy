e1de2d8022d54c1f442e1bb2bfdf045fa841ad33
t734728 <wallace.noronha@santander.com.br>
2020-12-29 09:34:58 -0300

    First commit

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,37 +1,42 @@
 import io.jenkins.plugins.KafkaProducerMessage
-def call(body){
 
-    def message = "Hello World!"
-    KafkaProducerMessage kafka = new KafkaProducerMessage("kafka:9091","teste")
-    kafka.produce("Mensagem de teste")
-
-
-    pipeline {
-
-        stages {
-            stage('Build') {
-                steps {
-
-                    echo 'Building..'
-                }
-            }
-            stage('Test') {
-                steps {
-                    echo 'Testing..'
-                }
-            }
-            stage('Print Message') {
-                steps {
-                    echo message
-                }
-            }
-            stage('Deploy') {
-                steps {
-                    echo 'Deploying....'
-                }
-            }
-        }
+node {
+    stage('Build') {
+        KafkaProducerMessage kafka = new KafkaProducerMessage("kafka:9091", "teste")
+        kafka.produce("Mensagem de teste")
     }
 }
-
-return this
+//def call(body){
+//
+//    def message = "Hello World!"
+//
+//
+//    pipeline {
+//
+//        stages {
+//
+//                steps {
+//
+//                    echo 'Building..'
+//                }
+//            }
+//            stage('Test') {
+//                steps {
+//                    echo 'Testing..'
+//                }
+//            }
+//            stage('Print Message') {
+//                steps {
+//                    echo message
+//                }
+//            }
+//            stage('Deploy') {
+//                steps {
+//                    echo 'Deploying....'
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//return this
